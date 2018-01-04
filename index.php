9b5c808c2775d47dc20144983d3043df5af3de10
dedsec404 <muhammad.wahidin@students.amikom.ac.id>
2018-01-03 21:48:12 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,223 @@
+<!DOCTYPE html>
+<html >
+<head>
+  <!-- Site made with Mobirise Website Builder v4.5.4, https://mobirise.com -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v4.5.4, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="https://rawgit.com/dedsec404/MFcloud/master/assets/images/logo4.png" type="image/x-icon">
+  <meta name="description" content="">
+  <title>MFcloud</title>
+  <link rel="stylesheet" href="https://rawgit.com/dedsec404/MFcloud/master/assets/web/assets/mobirise-icons/mobirise-icons.css">
+  <link rel="stylesheet" href="https://rawgit.com/dedsec404/MFcloud/master/assets/tether/tether.min.css">
+  <link rel="stylesheet" href="https://rawgit.com/dedsec404/MFcloud/master/assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://rawgit.com/dedsec404/MFcloud/master/assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="https://rawgit.com/dedsec404/MFcloud/master/assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="https://rawgit.com/dedsec404/MFcloud/master/assets/socicon/css/styles.css">
+  <link rel="stylesheet" href="https://rawgit.com/dedsec404/MFcloud/master/assets/theme/css/style.css">
+  <link rel="stylesheet" href="https://rawgit.com/dedsec404/MFcloud/master/assets/mobirise/css/mbr-additional.css" type="text/css">
+  
+</head>
+<body>
+<section class="cid-qFJIFrbpHZ mbr-fullscreen mbr-parallax-background" id="header2-0">
+
+    
+
+    
+
+    <div class="container align-center">
+        <div class="row justify-content-md-center">
+            <div class="mbr-white col-md-10">
+                <h1 class="mbr-section-title mbr-bold pb-3 mbr-fonts-style display-1">
+                    MFcloud</h1>
+                
+                <p class="mbr-text pb-3 mbr-fonts-style display-5">
+                    MFcloud Adalah Private File Hosting yang di miliki oleh <a href="https://mfnime.web.id" target="_blank"><strong>MFnime</strong></a>. Kami Sangat Menyarankan Download via <strong>MFcloud </strong>di banding dengan Server lain Seperti <strong>Google Drive</strong>,<strong> KBagi</strong>, dan lainnya. karena <strong>MFcloud </strong>sudah di rancang sedemikian rupa dan memiliki banyak kelebihan. diantaranya adalah:<br>"<strong>Support Resume</strong>, <strong>Terproteksi</strong>, <strong>Full Speed</strong>, <strong>No&nbsp;Ads</strong>, <strong>SSD</strong>, dan banyak lagi"</p>
+                <div class="mbr-section-btn"><a class="btn btn-md btn-secondary display-4" href="https://mfnime.web.id">Anime Batch</a>
+                    <a class="btn btn-md btn-white-outline display-4" href="https://mfnime.web.id">MFnime</a></div>
+            </div>
+        </div>
+    </div>
+    <div class="mbr-arrow hidden-sm-down" aria-hidden="true">
+        <a href="#next">
+            <i class="mbri-down mbr-iconfont"></i>
+        </a>
+    </div>
+</section>
+
+<section class="engine"><a href="https://mobirise.co/m">bootstrap table</a></section><section class="features16 cid-qFJL9h6eR8" id="features16-2">
+    
+    
+    
+    <div class="container align-center">
+        <h2 class="pb-3 mbr-fonts-style mbr-section-title display-2">
+            OUR AWESOME TEAM
+        </h2>
+        
+        <div class="row media-row">
+            
+        <div class="team-item col-lg-3 col-md-6">
+                <div class="item-image">
+                    <img src="https://rawgit.com/dedsec404/MFcloud/master/assets/images/noimage-462x450.jpg" alt="" title="">
+                </div>
+                <div class="item-caption py-3">
+                    <div class="item-name px-2">
+                        <p class="mbr-fonts-style display-5">
+                           Akikazu</p>
+                    </div>
+                    <div class="item-role px-2">
+                        <p>Developer</p>
+                    </div>
+                    <div class="item-social pt-2">
+                        <a href="#" target="_blank">
+                            <span class="p-1 socicon-twitter socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                        <a href="#" target="_blank">
+                            <span class="p-1 socicon-facebook socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                        <a href="#" target="_blank">
+                            <span class="p-1 socicon-googleplus socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                        <a href="#" target="_blank">
+                            <span class="p-1 socicon-linkedin socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                        <a href="#" target="_blank">
+                            
+                        </a>
+                        <a href="https://www.youtube.com/channel/UCt_tncVAetpK5JeM8L-8jyw" target="_blank">    
+                            
+                        </a>
+                    </div>
+                </div>
+            </div><div class="team-item col-lg-3 col-md-6">
+                <div class="item-image">
+                    <img src="https://rawgit.com/dedsec404/MFcloud/master/assets/images/noimage-450x462.jpg" alt="" title="">
+                </div>
+                <div class="item-caption py-3">
+                    <div class="item-name px-2">
+                        <p class="mbr-fonts-style display-5">
+                           MFnime</p>
+                    </div>
+                    <div class="item-role px-2">
+                        <p>Developer</p>
+                    </div>
+                    <div class="item-social pt-2">
+                        <a href="#" target="_blank">
+                            <span class="p-1 socicon-twitter socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                        <a href="#" target="_blank">
+                            <span class="p-1 socicon-facebook socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                        <a href="#" target="_blank">
+                            <span class="p-1 socicon-googleplus socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                        <a href="#" target="_blank">
+                            <span class="p-1 socicon-linkedin socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                        <a href="#" target="_blank">
+                            
+                        </a>
+                        <a href="https://www.youtube.com/channel/UCt_tncVAetpK5JeM8L-8jyw" target="_blank">    
+                            
+                        </a>
+                    </div>
+                </div>
+            </div><div class="team-item col-lg-3 col-md-6">
+                <div class="item-image">
+                    <img src="https://rawgit.com/dedsec404/MFcloud/master/assets/images/noimage-1-462x450.jpg" alt="" title="">
+                </div>
+                <div class="item-caption py-3">
+                    <div class="item-name px-2">
+                        <p class="mbr-fonts-style display-5">
+                           MFcloud</p>
+                    </div>
+                    <div class="item-role px-2">
+                        <p>Developer</p>
+                    </div>
+                    <div class="item-social pt-2">
+                        <a href="#" target="_blank">
+                            <span class="p-1 socicon-twitter socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                        <a href="#" target="_blank">
+                            <span class="p-1 socicon-facebook socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                        <a href="#" target="_blank">
+                            <span class="p-1 socicon-googleplus socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                        <a href="#" target="_blank">
+                            <span class="p-1 socicon-linkedin socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                        <a href="#" target="_blank">
+                            
+                        </a>
+                        <a href="https://www.youtube.com/channel/UCt_tncVAetpK5JeM8L-8jyw" target="_blank">    
+                            
+                        </a>
+                    </div>
+                </div>
+            </div><div class="team-item col-lg-3 col-md-6">
+                <div class="item-image">
+                    <img src="https://rawgit.com/dedsec404/MFcloud/master/assets/images/noimage-1-450x462.jpg" alt="" title="">
+                </div>
+                <div class="item-caption py-3">
+                    <div class="item-name px-2">
+                        <p class="mbr-fonts-style display-5">Izul Wahidin</p>
+                    </div>
+                    <div class="item-role px-2">
+                        <p>Developer</p>
+                    </div>
+                    <div class="item-social pt-2">
+                        <a href="#" target="_blank">
+                            <span class="p-1 socicon-twitter socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                        <a href="#" target="_blank">
+                            <span class="p-1 socicon-facebook socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                        <a href="#" target="_blank">
+                            <span class="p-1 socicon-googleplus socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                        <a href="#" target="_blank">
+                            <span class="p-1 socicon-linkedin socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                        <a href="#" target="_blank">
+                            
+                        </a>
+                        <a href="https://www.youtube.com/channel/UCt_tncVAetpK5JeM8L-8jyw" target="_blank">    
+                            
+                        </a>
+                    </div>
+                </div>
+            </div></div>    
+    </div>
+</section>
+
+<section once="" class="cid-qFJLhGP3HU" id="footer6-3">
+
+    
+
+    
+
+    <div class="container">
+        <div class="media-container-row align-center mbr-white">
+            <div class="col-12">
+                <p class="mbr-text mb-0 mbr-fonts-style display-7">
+                    © Copyright 2018 MFcloud - All Rights Reserved
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+  <script src="https://rawgit.com/dedsec404/MFcloud/master/assets/web/assets/jquery/jquery.min.js"></script>
+  <script src="https://rawgit.com/dedsec404/MFcloud/master/assets/popper/popper.min.js"></script>
+  <script src="https://rawgit.com/dedsec404/MFcloud/master/assets/tether/tether.min.js"></script>
+  <script src="https://rawgit.com/dedsec404/MFcloud/master/assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="https://rawgit.com/dedsec404/MFcloud/master/assets/smoothscroll/smooth-scroll.js"></script>
+  <script src="https://rawgit.com/dedsec404/MFcloud/master/assets/parallax/jarallax.min.js"></script>
+  <script src="https://rawgit.com/dedsec404/MFcloud/master/assets/theme/js/script.js"></script>
+  
+  
+</body>
+</html>
